bf0116c97098c0921685377a0d29cd0a306ac863
liheng <liheng19@jd.com>
2018-07-23 12:02:34 +0800

    增加helloWorld的Demo

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping(value = "/hello")
     public String sayHello() {
-        return "Hello Spring Boot!";
+        return "Hello Spring Boot! + Hello！";
     }
 }
