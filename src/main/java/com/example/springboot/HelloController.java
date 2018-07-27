56d55f5a92f168908f9e800181818273ae52358c
liheng <liheng19@jd.com>
2018-07-27 10:20:53 +0800

    不知道更新了什么

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,10 +3,14 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController {
     @RequestMapping(value = "/hello")
-    public String sayHello() {
-        return "Hello Spring Boot! + Hello！";
+    public String sayHello(HttpServletRequest request) {
+        String pin = request.getParameter("pin");
+        int channel = Integer.parseInt(request.getParameter("channel"));
+        return pin+channel;
     }
 }
